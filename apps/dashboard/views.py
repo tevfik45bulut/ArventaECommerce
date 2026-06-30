4c73239b491658f1c721c9b5bdc08ead8f68ae39
Tevfik <tevfik45bulut@gmail.com>
2026-06-30 15:54:01 +0300

    güncelleme yapıldı

--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -9,6 +9,43 @@ from django.views.generic import UpdateView
 from apps.dashboard.mixins import DashboardAccessMixin
 from apps.products.models import Product
 
+from django.views.generic import TemplateView
+
+from apps.brands.models import Brand
+from apps.categories.models import Category
+from apps.orders.models import Order
+
+from django.views.generic import TemplateView
+
+from apps.brands.models import Brand
+from apps.categories.models import Category
+from apps.orders.models import Order
+
+
+class DashboardView(DashboardAccessMixin, TemplateView):
+    template_name = "dashboard/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context["product_count"] = Product.objects.count()
+        context["category_count"] = Category.objects.count()
+        context["brand_count"] = Brand.objects.count()
+        context["order_count"] = Order.objects.count()
+
+        context["latest_products"] = (
+            Product.objects
+            .select_related("category", "brand")
+            .order_by("-id")[:10]
+        )
+
+        context["latest_orders"] = (
+            Order.objects
+            .order_by("-id")[:10]
+        )
+
+        return context
+
 
 class DashboardProductListView(DashboardAccessMixin, ListView):
     model = Product
